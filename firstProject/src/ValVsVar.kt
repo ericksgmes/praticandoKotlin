cd02609f56a5cddc20aa538cfd3c61fe1b83b830
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-04-17 09:17:53 -0300

    Feat(ValVsVar): Add file to see the difference of val and var

---
+++ firstProject/src/ValVsVar.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var brandVar = "brandVar"
+    brandVar = "Brand"
+    // final String brand
+    val brandVal = "brandVal" // cannot be changed
+    // brandVal = "Brand" nonono
+}
