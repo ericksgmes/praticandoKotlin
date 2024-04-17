0b553d7a91cec07f3e69a747fac065df071cb52e
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-04-17 09:00:56 -0300

    Feat(Println): Add file to see print types

---
+++ firstProject/src/Println.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // println("Hello please comment me")
+    /*
+    test multi line comments
+    println("Multi line comments")
+    */
+    print("hello! ")
+    println("how are you?")
+}
