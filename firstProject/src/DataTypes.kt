66ed3a28201a5bd2e52aa965a642646a2f7fc7e7
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-04-17 09:09:36 -0300

    Feat(DataTypes): Add file to see different data types

---
+++ firstProject/src/DataTypes.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var number: Int = 19
+    var bigNumber: Long = 100L
+    var decimalNumber: Double = 3.14
+    var bigdecimalNumber: Float = 3.14F
+    var booleano: Boolean = true //or false
+    var string: String = "Hello World"
+    var caracteres: Char = 'U'
+
+}
