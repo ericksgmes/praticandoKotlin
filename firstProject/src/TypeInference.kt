f27983a64f914507acd9a5da4ce4a7e3f9b79e9a
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-04-17 09:15:32 -0300

    Feat(TypeInference): Add file to see that Kt infers data types

---
+++ firstProject/src/TypeInference.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var number = 19
+    var bigNumber = 100L
+    var decimalNumber = 3.14
+    var bigdecimalNumber = 3.14F
+    var booleano = true //or false
+    var string = "Hello World"
+    var caracteres = 'U'
+}
