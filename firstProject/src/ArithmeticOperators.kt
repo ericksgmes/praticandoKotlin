4a9232c3592c6265d5eb66a99fddc7c2658a0142
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-04-18 15:37:20 -0300

    feat(ArithmeticOperators): Add file to see how operators work

---
+++ firstProject/src/ArithmeticOperators.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val number1 = 2;
+    val number2 = 3;
+    println(number1 + number2)
+    println(number1 - number2)
+    println(number1 * number2)
+    println(number1 / number2)
+    println(number1 % number2)
+}
