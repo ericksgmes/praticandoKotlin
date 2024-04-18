86135a7eab0bd21e0ea7d22afb85b52ee688ad36
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-04-18 15:55:23 -0300

    feat(IncDecOperators): Add file to see how to work with increase and decrease operators

---
+++ firstProject/src/IncDecOperators.kt
@@ -0,0 +1,8 @@
+fun main() {
+    // ++ and --
+    var number = 10
+    println(++number) // increments and then returns
+    println(number++) // returns and then increments
+    println(--number)
+    println(number--)
+}
